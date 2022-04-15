8895fd0d1769f445357cd8d607919acaa5e82b05
Sam Brown <jumpbro@hotmail.com>
2022-04-15 19:42:54 +1000

    modal added

--- js/main.js
+++ js/main.js
@@ -1,10 +1,27 @@
 const hamburger = document.querySelector('.hamburger')
 const mobileMenu = document.querySelector('.mobile-nav')
+const projectsModal = document.querySelector('#projectsModal')
+const projectsModalBtn = document.querySelector('#viewProjects')
+const closeProjectsModal = document.querySelector('#closeProjectsModal')
 
 hamburger.addEventListener('click', function(){
     this.classList.toggle('isActive')
     mobileMenu.classList.toggle('isActive')
 })
+
+projectsModalBtn.addEventListener('click', function(){
+    projectsModal.style.display = 'block'
+})
+
+closeProjectsModal.addEventListener('click', function(){
+    projectsModal.style.display = 'none'
+})
+
+// window.addEventListener('click', function(){
+//     if(this.target == projectsModal){{
+//         projectsModal.style.display ='none'
+//     }}
+// })
 
 
 // window.onload = function(){
