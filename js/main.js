44ae9e702cdc1e3575e95f7e8aa480f610926712
Sam Brown <jumpbro@hotmail.com>
2022-04-15 15:09:12 +1000

    creating hero section and services grid

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
 const hamburger = document.querySelector('.hamburger')
+const mobileMenu = document.querySelector('.mobile-nav')
 
 hamburger.addEventListener('click', function(){
     this.classList.toggle('isActive')
+    mobileMenu.classList.toggle('isActive')
 })
+
+
+// window.onload = function(){
+//     const mobileMenu = document.querySelector('.mobile-nav')
+
+
+
+// }
